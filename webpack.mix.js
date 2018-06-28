c888fb0704b4e95e1f43953b789b94697057c8ff
Artenair <gattosamurai@gmail.com>
2018-06-28 17:48:59 +0200

    Changed laravel mix to copy images

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 require('laravel-mix-tailwind');
 
-mix.js('resources/js/app.js', 'assets/js')
-    .sass('resources/scss/app.scss', 'assets/css')
+mix.js('resources/js/app.js', 'public/assets/js')
+    .sass('resources/scss/app.scss', 'public/assets/css')
+    .copy('resources/img', 'public/assets/img')
     .tailwind();
