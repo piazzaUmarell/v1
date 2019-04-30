f38a31d84de0bfda90304a85c1b26ae4829662b2
Danilo Allasia <danilo.allasia@netatlas.it>
2019-04-30 09:57:40 +0200

    Styled filter to be responsive on scroll.

--- resources/js/Constants.ts
+++ resources/js/Constants.ts
@@ -3,6 +3,5 @@ export default class Constants {
     static ROUTE_EPISODE_SHOW: string = "page_episode_show";
 
     static API_EPISODE_INDEX: string = "episodes.index";
-
     static API_LATEST_EPISODE_SHOW: string = "latest-episode.show";
 }
--- resources/js/components/EpisodeIndexFilter.vue
+++ resources/js/components/EpisodeIndexFilter.vue
@@ -1,5 +1,8 @@
 <template>
-    <header class="filter">
+    <header class="filter lg:flex px-8 lg:px-32" :class="{sticky: scrolledPast}">
+        <router-link :to="homepageRoute" class="hover:no-underline no-underline">
+            <h1 class="lg:mx-16">Piazza Umarell</h1>
+        </router-link>
         <div class="input-container">
             <label class="ml-4" for="filter">
                 <font-awesome-icon icon="search" class="text-grey-darker"></font-awesome-icon>
@@ -18,10 +21,9 @@
 <script>
 
     import EpisodeAccessor from "../mixins/EpisodesAccessor";
-
+    import Constants from "../Constants";
     import { faSearch } from '@fortawesome/free-solid-svg-icons'
     import { library } from '@fortawesome/fontawesome-svg-core';
-    import KeywordCollection from "../api/KeywordCollection";
 
     export default {
         name: "EpisodeIndexFilter",
@@ -29,7 +31,8 @@
 
         data() {
             return {
-                keywords: ""
+                keywords: "",
+                scrolledPast: false,
             }
         },
 
@@ -37,22 +40,64 @@
             library.add(faSearch);
         },
 
+        mounted() {
+            window.addEventListener(
+                'scroll',
+                this.handleScroll.bind(this)
+            );
+        },
+
+        destroyed () {
+            window.removeEventListener(
+                'scroll',
+                this.handleScroll.bind(this)
+            );
+        },
+
         methods: {
             triggerFilter() {
                 this.applyKeywords(
                     this.keywords
                 );
+            },
+
+            handleScroll() {
+                this.scrolledPast = window.scrollY >= this.$el.offsetTop;
+            },
+        },
+
+        computed: {
+            homepageRoute() {
+                return {name: Constants.ROUTE_HOMEPAGE}
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+
     .filter {
-        @apply text-3xl mb-8 sticky pin-t bg-white py-4 w-full px-32;
+
+        @apply text-3xl mb-32 bg-white py-6 w-full items-baseline;
+
+        &.sticky {
+            @apply sticky pin-t pl-8;
+            background: linear-gradient(to right, config("colors.blue-dark"), config("colors.indigo-light"));
+
+            h1 {
+                display: block;
+            }
+        }
+
+        h1 {
+            display: none;
+            content:"Piazza Umarell";
+            font-family: "Pridi", serif;
+            @apply py-4 text-white rounded-full text-5xl text-center;
+        }
 
         .input-container {
-            @apply rounded-full border border-grey-lighter bg-grey-lighter px-8 outline-none pt-4 pb-2 flex items-baseline;
+            @apply rounded-full border border-grey-lighter bg-grey-lighter px-8 outline-none pt-4 pb-2 flex items-baseline flex-1;
 
             input {
                 @apply outline-none pl-4 flex-1;
